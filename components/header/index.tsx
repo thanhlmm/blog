0bf5319ceed926d14e88339eefded25f3a26cdc1
Thanh Le <mihthanh27@gmail.com>
2021-10-17 18:02:58 +0700

    Fix build error

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -21,7 +21,7 @@ const navigation = [
 //   { name: "Sign out", href: "#" },
 // ];
 
-function classNames(...classes) {
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
