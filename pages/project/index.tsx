3c38537431e9a4e2b0ec9339f099e737d10fbcc5
Thanh Le <mihthanh27@gmail.com>
2021-11-08 22:57:38 +0700

    Fix story behind button

--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -90,7 +90,7 @@ const ProjectPage = ({ projects }: { projects: Project[] }) => {
                             </a>
                           )}
 
-                          {prj?.url?.length && prj?.url?.length > 10 && (
+                          {prj.story && (
                             <a
                               href={prj.story}
                               className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-indigo-700 border border-transparent rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
