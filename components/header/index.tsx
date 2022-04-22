e702306e0443e71f26680633325dde03e2a6c806
Thanh Le <mihthanh27@gmail.com>
2022-04-22 14:41:53 +0700

    Change menu hover color

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -141,7 +141,7 @@ export default function Header() {
                     className={classNames(
                       router.pathname.includes(item.href)
                         ? "bg-blue-50 border-blue-500 text-blue-700 dark:bg-gray-700 dark:text-white"
-                        : "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 dark:text-gray-400",
+                        : "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-gray-700",
                       "block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
                     )}
                     aria-current={router.pathname.includes(item.href)}
