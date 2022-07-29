5b4754b91247da28df5b1b26d21d2707a4544f11
Thanh Le <mihthanh27@gmail.com>
2022-07-29 08:54:55 +0000

    Fix menu highlight

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -55,7 +55,7 @@ export default function Header() {
                     <Link key={item.name} href={item.href}>
                       <a
                         className={classNames(
-                          router.pathname.includes(item.href)
+                          router.pathname === item.href
                             ? "border-blue-500 text-gray-900 dark:text-white"
                             : "border-transparent text-gray-500 dark:text-gray-300 hover:border-gray-300 hover:text-gray-700",
                           "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
@@ -139,7 +139,7 @@ export default function Header() {
                     key={item.name}
                     href={item.href}
                     className={classNames(
-                      router.pathname.includes(item.href)
+                      router.pathname === item.href
                         ? "bg-blue-50 border-blue-500 text-blue-700 dark:bg-gray-700 dark:text-white"
                         : "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 dark:text-gray-400 dark:hover:bg-gray-700",
                       "block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
