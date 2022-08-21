3af7095d6057ae36397ebba6f9f292cd47411f0b
Thanh Le <mihthanh27@gmail.com>
2022-08-21 10:42:25 +0700

    Change index style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { CalendarIcon, ChevronRightIcon } from "@heroicons/react/solid";
 import dayjs from "dayjs";
 import { GetStaticProps } from "next";
 import Head from "next/head";
@@ -79,69 +78,47 @@ function HomePage({ posts }: { posts: Post[] }) {
         loading="lazy"
       ></iframe> */}
 
-      <div className="overflow-hidden bg-white shadow dark:bg-gray-800 sm:rounded-md">
-        <ul
-          role="list"
-          className="divide-y divide-gray-200 dark:divide-gray-900"
-        >
+      <h2 className="mb-2 text-xl font-semibold">Posts</h2>
+      <div className="mb-8 text-gray-700 dark:text-gray-100">
+        I share about Frontend, Engineer and some thought in product and life 😴
+      </div>
+
+      <div className="overflow-hidden">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
           {posts.map((post) => (
-            <li key={post.id}>
+            <div
+              key={post.id}
+              className="transition-all bg-white border border-gray-200 hover:border-blue-400 dark:hover:border-blue-400 dark:border-gray-500 dark:bg-gray-800"
+            >
               <Link href={`/blog/${post.slug}`} locale={post.lang}>
-                <a className="block hover:bg-gray-50 dark:hover:bg-gray-700">
-                  <div className="flex items-center px-4 py-4 sm:px-6">
-                    <div className="flex-1 min-w-0 sm:flex sm:items-center sm:justify-between">
-                      <div className="truncate">
-                        <div className="flex text-sm">
-                          <p className="font-medium text-blue-600 truncate dark:text-blue-500">
-                            {post.title}
-                          </p>
-                        </div>
-                        <div className="mt-1 space-x-1">
-                          {post.tag?.map((tag) => (
-                            <span
-                              key={tag}
-                              className="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-yellow-100 text-yellow-800"
-                            >
-                              {tag}
-                            </span>
-                          ))}
-                        </div>
-                        <div className="flex mt-2">
-                          <div className="flex items-center text-sm text-gray-500 dark:text-gray-100">
-                            <CalendarIcon
-                              className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400 dark:text-gray-200"
-                              aria-hidden="true"
-                            />
-                            <p>
-                              <time dateTime={post.date}>{post.date}</time>
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="flex-shrink-0 mt-4 sm:mt-0 sm:ml-5">
-                        <div className="flex -space-x-1 overflow-hidden">
-                          {/* <Image
-                          src="/me.jpeg"
-                          alt="me"
-                          className="rounded-md"
-                          width="40"
-                          height="40"
-                        /> */}
-                        </div>
-                      </div>
+                <a className="block">
+                  <div className="px-4 py-4 sm:px-6">
+                    <div className="flex items-center mb-2 text-xs text-gray-400 dark:text-gray-100">
+                      <time dateTime={post.date}>
+                        {dayjs(post.date, "YYYY-MM-DD").format("MMM DD, YYYY")}
+                      </time>
                     </div>
-                    <div className="flex-shrink-0 ml-5">
-                      <ChevronRightIcon
-                        className="w-5 h-5 text-gray-400"
-                        aria-hidden="true"
-                      />
+                    <div className="flex text-sm">
+                      <p className="mb-3 font-medium text-blue-600 dark:text-blue-500">
+                        {post.title}
+                      </p>
+                    </div>
+                    <div className="space-x-1">
+                      {post.tag?.map((tag) => (
+                        <span
+                          key={tag}
+                          className="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-yellow-100 text-yellow-800"
+                        >
+                          {tag}
+                        </span>
+                      ))}
                     </div>
                   </div>
                 </a>
               </Link>
-            </li>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
