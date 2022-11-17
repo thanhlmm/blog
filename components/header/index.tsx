b92f1ff414b83902e60a9f5b75e44ed2131ba7a7
Thanh Le <mihthanh27@gmail.com>
2022-11-17 06:59:49 +0000

    Add link

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -47,7 +47,7 @@ export default function Header() {
               <div className="flex">
                 <div className="flex items-center flex-shrink-0">
                   <Link href="/">
-                    <a className="text-gray-900  dark:text-gray-50">
+                    <a className="text-gray-900 dark:text-gray-50">
                       ThanhLe's blog
                     </a>
                   </Link>
@@ -101,6 +101,23 @@ export default function Header() {
                       Tiếng Việt
                     </a>
                   </div>
+                </div>
+                <div className="mx-4 text-sm font-medium text-orange-500">
+                  <a href="/rss" target="_blank">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="inline-block w-5 h-5 mr-1"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3.75 4.5a.75.75 0 01.75-.75h.75c8.284 0 15 6.716 15 15v.75a.75.75 0 01-.75.75h-.75a.75.75 0 01-.75-.75v-.75C18 11.708 12.292 6 5.25 6H4.5a.75.75 0 01-.75-.75V4.5zm0 6.75a.75.75 0 01.75-.75h.75a8.25 8.25 0 018.25 8.25v.75a.75.75 0 01-.75.75H12a.75.75 0 01-.75-.75v-.75a6 6 0 00-6-6H4.5a.75.75 0 01-.75-.75v-.75zm0 7.5a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    RSS
+                  </a>
                 </div>
                 <div className="relative ml-3">
                   <div>
