836c520260653b55667800bcd2cb0e399fd4f544
Thanh Le <mihthanh27@gmail.com>
2022-12-20 18:31:32 +0700

    Add footer banner

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,7 +2,7 @@ import { XIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 
 const Footer = () => {
-  const [showBanner, toogleShowBanner] = useState(false);
+  const [showBanner, toogleShowBanner] = useState(true);
 
   return (
     <>
@@ -21,31 +21,23 @@ const Footer = () => {
       {showBanner && (
         <div className="fixed inset-x-0 bottom-0 pb-2 sm:pb-5">
           <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div className="p-1 bg-green-500 rounded-lg shadow-lg sm:p-2">
+            <div className="p-1 rounded-lg shadow-lg bg-amber-500 sm:p-2">
               <div className="flex flex-wrap items-center justify-between">
                 <div className="flex items-center flex-1 w-0">
-                  <span className="flex p-1 text-2xl bg-green-500 rounded-lg">
-                    🥳
+                  <span className="flex p-1 text-2xl rounded-lg bg-amber-500">
+                    🤓
                   </span>
                   <p className="ml-3 font-medium text-white">
                     <span>
-                      Mình có{" "}
-                      <a
-                        className="underline underline-offset-2 "
-                        href="https://www.facebook.com/thanh.lmm/posts/pfbid025PW9qQXq9rSzmXMrbe7JmjHZZQ3DzYnvGbBYu325BwqVY4BEE13SazToTTLncQ3Rl?__cft__[0]=AZWgh-nZ7ZYeNvkoMuER9uJfTTbVHSe8RS2r1Ov673Z1g6_qzyYJrz5LA7e0ycPYjb8SEbJNIP6BmrUim4Lv3oRoNeAVXeU_Vdzozp_kN8_2ltR79zwCmCjlb7il7pS3jLCxg0pc9pRpIhf0EtTw3swLnSdwgnTgveuJXcT6pLzri2WhWzHowIzVQF8w9B3J0s4&__tn__=%2CO%2CP-R"
-                      >
-                        buổi chia sẻ về Career, Mindset và một chút về
-                        Blockchain
-                      </a>{" "}
-                      vào thứ 2, mọi người vào nghe thử xem thằng Thành này nghĩ
-                      gì nhé!
+                      Sắp tới mình sẽ viết 2 tuần một bài để focus sâu hơn vào
+                      nội dung. Hy vọng mọi người vẫn ủng hộ nhé!
                     </span>
                   </p>
                 </div>
                 <div className="flex-shrink-0 order-2 sm:order-3 sm:ml-2">
                   <button
                     type="button"
-                    className="flex p-2 -mr-1 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-white"
+                    className="flex p-2 -mr-1 rounded-md hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-white"
                     onClick={() => toogleShowBanner(false)}
                   >
                     <span className="sr-only">Dismiss</span>
