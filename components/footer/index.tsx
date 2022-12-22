9d8776ebddb8b12e70e080b8fa8e40238d99be72
Thanh Le <mihthanh27@gmail.com>
2022-12-22 10:40:44 +0700

    Change banner

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,8 +1,14 @@
 import { XIcon } from "@heroicons/react/outline";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Footer = () => {
-  const [showBanner, toogleShowBanner] = useState(true);
+  const [showBanner, toogleShowBanner] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      toogleShowBanner(true);
+    }, 1000);
+  }, []);
 
   return (
     <>
@@ -18,37 +24,48 @@ const Footer = () => {
           </a>
         </p>
       </div>
-      {showBanner && (
-        <div className="fixed inset-x-0 bottom-0 pb-2 sm:pb-5">
-          <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div className="p-1 rounded-lg shadow-lg bg-amber-500 sm:p-2">
-              <div className="flex flex-wrap items-center justify-between">
-                <div className="flex items-center flex-1 w-0">
-                  <span className="flex p-1 text-2xl rounded-lg bg-amber-500">
-                    🤓
+      <div
+        className={[
+          "fixed inset-x-0 pb-2 sm:pb-5 transition-all",
+          showBanner ? "bottom-0 opacity-100" : "bottom-[-30px] opacity-0",
+        ].join(" ")}
+      >
+        <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="p-1 bg-white border border-green-500 rounded-lg shadow-lg dark:bg-gray-700 border-1 sm:p-2">
+            <div className="flex flex-wrap items-center justify-between">
+              <div className="flex items-center flex-1 w-0 mx-3">
+                <span className="flex p-1 text-2xl rounded-lg">🤓</span>
+                <p className="ml-3 font-medium text-green-600">
+                  <span>
+                    Thứ 7 (24-12) lúc 9 giờ sáng mình có buổi workshop nhỏ chia
+                    sẻ cách viết Smart Contract dùng Solidity, target là chỉ cần
+                    biết code là làm được. <br /> Nếu bạn hứng thú hãy tham gia
+                    nhé!
                   </span>
-                  <p className="ml-3 font-medium text-white">
-                    <span>
-                      Sắp tới mình sẽ viết 2 tuần một bài để focus sâu hơn vào
-                      nội dung. Hy vọng mọi người vẫn ủng hộ nhé!
-                    </span>
-                  </p>
-                </div>
-                <div className="flex-shrink-0 order-2 sm:order-3 sm:ml-2">
-                  <button
+                </p>
+              </div>
+              <div className="flex-shrink-0 order-2 mr-2 sm:order-3 sm:ml-2">
+                <a
+                  type="button"
+                  href="https://discord.gg/Ay9PtBVe?event=1054623077788565575"
+                  target="_blank"
+                  className="flex p-2 -mr-1 text-white bg-green-600 rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+                >
+                  Tham gia
+                </a>
+                {/* <button
                     type="button"
-                    className="flex p-2 -mr-1 rounded-md hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-white"
+                    className="flex p-2 -mr-1 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-white"
                     onClick={() => toogleShowBanner(false)}
                   >
                     <span className="sr-only">Dismiss</span>
                     <XIcon className="w-6 h-6 text-white" aria-hidden="true" />
-                  </button>
-                </div>
+                  </button> */}
               </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
