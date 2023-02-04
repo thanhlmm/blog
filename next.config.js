44a6e1446480937bf7f68f374b8a271aa319e620
Thanh Le <mihthanh27@gmail.com>
2023-02-04 18:24:19 +0700

    Ignore ts error

--- next.config.js
+++ next.config.js
@@ -26,4 +26,11 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
